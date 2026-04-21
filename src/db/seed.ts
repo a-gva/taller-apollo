afb9a7ad0d7672104db8464da905b76a188689ec
Alexy Viegas <alexyviegas@gmail.com>
2026-04-21 18:04:58 -0300

    seed

---
+++ src/db/seed.ts
@@ -0,0 +1,22 @@
+import { mockedListUsers } from '../tests/mockedListUsers';
+import { db } from './client';
+import { users } from './schema';
+
+async function run() {
+  const result = await db
+    .insert(users)
+    .values(mockedListUsers.data)
+    .onConflictDoNothing()
+    .returning();
+  console.log(
+    `Seed complete. Inserted ${result.length} new users, skipped ${
+      mockedListUsers.data.length - result.length
+    } existing users.`,
+  );
+}
+
+run().catch((error) => {
+  console.error('Seed failed.');
+  console.error(error);
+  process.exit(1);
+});
